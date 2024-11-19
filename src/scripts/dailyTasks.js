e9a5d7f1776c03a8ef2fcab03b5930308280c3d6
Kris Mendoza <thekittengallery@gmail.com>
2024-11-20 01:09:20 +0800

    Refactor setRecurring method to accept days array

--- src/scripts/dailyTasks.js
+++ src/scripts/dailyTasks.js
@@ -1,8 +1,8 @@
 import { Task } from './tasks';
 
 class DailyTask extends Task {
-  setRecurring(id, frequency) {
-    this.tasksList[this.findByID(id)].recurring = frequency;
+  setRecurring(id, days) {
+    this.tasksList[this.findByID(id)].recurrence = days;
   }
 }
 
@@ -20,6 +20,6 @@ dailyTask.edit(3, 'maybe eat once more', 'before sleep', '12-11-2024', '03:30');
 dailyTask.markComplete(2);
 // dailyTask.markIncomplete(2);
 dailyTask.rearrange(4, 0);
-dailyTask.setRecurring(4, 'daily');
+dailyTask.setRecurring(4, ['monday', 'tuesday', 'wednesday']);
 
 dailyTask.log();

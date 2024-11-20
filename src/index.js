e60c1d5337391a7b93113fd7ae375a1cc7fb2924
Kris Mendoza <thekittengallery@gmail.com>
2024-11-21 04:36:10 +0800

    Add initial styling for the page structure - Add main css file for styling - Fix HTML structure by moving elements out of the header section - Add classes and containers to HTML

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 import { dailyTask } from './scripts/dailyTasks';
 import { weeklyTask } from './scripts/weeklyTasks';
 import { monthlyTask } from './scripts/monthlyTasks';
+import styles from './styles/main.css';

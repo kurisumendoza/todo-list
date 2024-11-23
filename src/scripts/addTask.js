54659661a8a673d61e472bb0d1b556b872606452
Kris Mendoza <thekittengallery@gmail.com>
2024-11-24 05:35:51 +0800

    Improve task dialog interaction - Change the OK button to an input type submit and also update its class - Add an OK button on the main input box that opens dialog when clicked - Make the OK button visible/hidden based on the input's presence - Add a function that migrates the input into the dialog form - Implement minimal style changes

--- src/scripts/addTask.js
+++ src/scripts/addTask.js
@@ -1,9 +1,32 @@
 import { TaskModal } from './taskModal';
-import { addTaskInput, taskModal } from './selectors';
+import { addTaskInput, addTaskBtn, taskModal } from './selectors';
 
 export const addTask = new TaskModal();
 
-addTaskInput.addEventListener('click', addTask.openModal);
+const addTextToDialog = function () {
+  taskModal.task.value = addTaskInput.value;
+  addTaskInput.value = '';
+};
+
+addTaskInput.addEventListener('input', () => {
+  if (addTaskInput.value.length < 1) return;
+  addTaskBtn.style.visibility = 'visible';
+});
+addTaskInput.addEventListener('keyup', (e) => {
+  if (e.key !== 'Backspace') return;
+  if (addTaskInput.value.length < 1) addTaskBtn.style.visibility = 'hidden';
+});
+addTaskInput.addEventListener('keydown', (e) => {
+  if (e.key !== 'Enter') return;
+  if (addTaskInput.value.length <= 0) return;
+  e.preventDefault();
+  addTask.openModal();
+  addTextToDialog();
+});
+addTaskBtn.addEventListener('click', () => {
+  addTask.openModal();
+  addTextToDialog();
+});
 taskModal.cancel.addEventListener('click', (e) => {
   e.preventDefault();
   addTask.closeModal();
--- src/scripts/selectors.js
+++ src/scripts/selectors.js
@@ -1,8 +1,10 @@
 export const tasksListContainer = document.querySelector('.tasks-list');
 
 export const addTaskInput = document.querySelector('.add-task');
+export const addTaskBtn = document.querySelector('.okay-btn');
 
 export const taskModal = {
   modal: document.querySelector('.task-details-modal'),
   cancel: document.querySelector('.cancel-btn'),
+  task: document.getElementById('task'),
 };

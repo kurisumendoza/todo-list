0d2db7e1655082e7f4f3cbc23dd24ef06da39a70
Kris Mendoza <thekittengallery@gmail.com>
2024-11-24 05:46:55 +0800

    Add a function to clear input and hide OK button - Add .trim() to eventListeners to avoid empty spaces being counted

--- src/scripts/addTask.js
+++ src/scripts/addTask.js
@@ -4,21 +4,27 @@ import { addTaskInput, addTaskBtn, taskModal } from './selectors';
 export const addTask = new TaskModal();
 
 const addTextToDialog = function () {
-  taskModal.task.value = addTaskInput.value;
+  taskModal.task.value = addTaskInput.value.trim();
+  clearInputText();
+};
+
+const clearInputText = function () {
   addTaskInput.value = '';
+  addTaskBtn.style.visibility = 'hidden';
 };
 
 addTaskInput.addEventListener('input', () => {
-  if (addTaskInput.value.length < 1) return;
+  if (addTaskInput.value.trim().length < 1) return;
   addTaskBtn.style.visibility = 'visible';
 });
 addTaskInput.addEventListener('keyup', (e) => {
   if (e.key !== 'Backspace') return;
-  if (addTaskInput.value.length < 1) addTaskBtn.style.visibility = 'hidden';
+  if (addTaskInput.value.trim().length < 1)
+    addTaskBtn.style.visibility = 'hidden';
 });
 addTaskInput.addEventListener('keydown', (e) => {
   if (e.key !== 'Enter') return;
-  if (addTaskInput.value.length <= 0) return;
+  if (addTaskInput.value.trim().length <= 0) return;
   e.preventDefault();
   addTask.openModal();
   addTextToDialog();
